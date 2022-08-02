f1ef96e98f78ea8d0c30889d73d210216c55e721
rihyun1126 <rihyun1126@gmail.com>
2022-08-02 17:54:36 +0900

    MainActivity Drawerlayout 생성

--- app/src/main/java/com/example/workers/MainActivity.kt
+++ app/src/main/java/com/example/workers/MainActivity.kt
@@ -3,25 +3,64 @@ package com.example.workers
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.workers.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import org.json.JSONObject
 import java.text.SimpleDateFormat
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle : ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
+        setSupportActionBar(binding.toolBar)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.drawer_open, R.string.drawer_close)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+        binding.mainDrawerView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.menuNotification -> {
+                    val intent = Intent(this, NotificationActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.menuSalary -> {
+                    val intent = Intent(this, SalaryActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.menuCalendar -> {
+                    val intent = Intent(this, CalendarActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.menuMyPage -> {
+                    val intent = Intent(this, MypageActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.logout -> {
+                    MyApplication.prefs.clear()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            true
+        }
 
         lateinit var name : String
         var status by Delegates.notNull<Int>()
@@ -42,6 +81,13 @@ class MainActivity : AppCompatActivity() {
 
                 name = array.getJSONObject(0).getString("name")
                 binding.mainName.text = name
+
+                val navigationView: NavigationView =  findViewById(R.id.main_drawer_view)
+                val headerView = navigationView.getHeaderView(0)
+                val navName : TextView = headerView.findViewById(R.id.navigationName)
+                val navEmplId : TextView = headerView.findViewById(R.id.navigationEmplId)
+                navName.text = name
+                navEmplId.text = MyApplication.prefs.getString("worker_id", "")
                 binding.mainContents.text = name + " 님은 " + today + "구역에서 까지 근무입니다."
 
                 status = array.getJSONObject(0).getString("status").toInt()
@@ -82,12 +128,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
 
-        binding.logoutBtn.setOnClickListener {
-            MyApplication.prefs.clear()
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) return true
+
+        return super.onOptionsItemSelected(item)
     }
 }
