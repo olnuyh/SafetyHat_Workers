f206a84c6df896a9cbee25dc467efd0f64664b54
rihyun1126 <rihyun1126@gmail.com>
2022-09-09 00:42:42 +0900

    공지,네비게이션 메뉴

--- app/src/main/java/com/example/workers/NotificationAdapter.kt
+++ app/src/main/java/com/example/workers/NotificationAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.workers
 
 import android.content.Context
+import android.util.SparseBooleanArray
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.workers.databinding.ItemNotificationBinding
@@ -10,6 +12,10 @@ import org.json.JSONObject
 
 class NotificationViewHolder(val binding : ItemNotificationBinding) : RecyclerView.ViewHolder(binding.root)
 class NotificationAdapter(val context : Context, val arr : JSONArray) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    // 선택 데이터 리스트
+    private var selectedItems: SparseBooleanArray = SparseBooleanArray()
+
     override fun getItemCount(): Int {
         return arr.length()?:0
     }
@@ -23,8 +29,35 @@ class NotificationAdapter(val context : Context, val arr : JSONArray) : Recycler
 
         val notification = arr[position] as JSONObject
         binding.itemTitle.text = notification.getString("notification_title")
+        binding.itemContent.text = notification.getString("notification_contents")
+        binding.itemName.text="관리자 "+notification.getString("notification_writer")
         val insert_date = notification.getString("insert_date")
         val date = insert_date.substring(0, insert_date.indexOf(" "))
-        binding.itemDate.text = date
+        binding.itemDate.text=date
+
+        binding.ivItemUpImg.setOnClickListener {
+            if (selectedItems.get(position)) {
+                // VISIBLE -> INVISIBLE
+                selectedItems.delete(position)
+
+                binding.clItemExpand.visibility = View.GONE
+            }
+            binding.ivItemUpImg.visibility= View.GONE
+            binding.ivItemDownImg.visibility= View.VISIBLE
+        }
+
+        binding.ivItemDownImg.setOnClickListener {
+
+            if (selectedItems.get(position)) {
+
+            } else {
+                // INVISIBLE -> VISIBLE
+                selectedItems.put(position, true)
+
+                binding.clItemExpand.visibility = View.VISIBLE
+            }
+            binding.ivItemDownImg.visibility= View.GONE
+            binding.ivItemUpImg.visibility= View.VISIBLE
+        }
     }
 }
